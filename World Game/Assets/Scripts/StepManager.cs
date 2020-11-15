0ca2aef7939d812e7f637577aa00c171b4f39eee
Patrick <pah.kocken@gmail.com>
2020-11-15 10:06:32 +0100

    Add Scene, GameManager and StepManager

---
+++ World Game/Assets/Scripts/StepManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class StepManager : MonoBehaviour
+{
+    public static StepManager Instance;
+
+    private DateTime _startDate;
+    private DateTime _currentDate = DateTime.Now;
+    private int _steps = 1;
+    private Coroutine _goingToNextStep;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(this);
+    }
+
+    private void StartGoingToNextStep()
+    {
+        _goingToNextStep = StartCoroutine(GoToNextStep());
+    }
+
+    public bool IsGoingToNextStep()
+    {
+        return _goingToNextStep != null;
+    }
+
+    IEnumerator GoToNextStep()
+    {
+        DateTime targetDate = _currentDate.AddDays(365);
+
+        while (_currentDate < targetDate)
+        {
+            _currentDate = _currentDate.AddDays(1);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        _steps++;
+        _goingToNextStep = null;
+    }
+}
